37b4dc7162c8aaad16bc881b2a9ddb31c3a6c414
Ivan Oliynyk <barjakall@gmail.com>
2024-02-14 21:38:02 +0000

    added columns to user

--- app/controllers/bites_controller.rb
+++ app/controllers/bites_controller.rb
@@ -1,2 +1,17 @@
 class BitesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+  #def new
+    #@bite = current_user.bites.new
+  #end
+  def index
+  end
+
+  def new
+    @bite = Bite.new
+  end
+
+  # def create
+  #   @bite = Bite.new(bite_params)
+  #   @bite.user = current_user
+  # end
 end
