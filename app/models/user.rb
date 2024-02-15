2b198a47d99d60bde387ec578ab4511508af520e
Ahmed Bouguacha <ahmad.bougacha@gmail.com>
2024-02-15 22:49:22 +0100

    Implemented new and create for BitesController

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # has_many :bites, dependent: :destroy
+  has_many :bites, dependent: :destroy
   # has_many :reviews, dependent: :destroy
   # has_many :guests
-  # validates_presence_of :first_name, :last_name, :address, :phone_number, :birth_date, :gender, :languages
-  # validates_uniqueness_of :phone_number
+  validates_presence_of :first_name, :last_name, :phone_number, :birth_date
+  validates :phone_number, uniqueness: true, format: { with: /\A[0-9]{10}\z/, message: "Please enter a valid phone number" }
 end
