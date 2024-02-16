68d2c34d125af2b6384ab828f5c9f9298c351709
Ahmed Bouguacha <ahmad.bougacha@gmail.com>
2024-02-17 00:05:28 +0100

    Added bite edit & update

--- app/models/bite.rb
+++ app/models/bite.rb
@@ -1,7 +1,7 @@
 class Bite < ApplicationRecord
   belongs_to :user
 
-  validates_presence_of :name, :date, :number_of_guests, :location, :address, :price, :accessibility
+  validates_presence_of :name, :date, :number_of_guests, :city, :address, :price, :accessibility
   validates_numericality_of :number_of_guests, greater_than_or_equal_to: 1
   validates_numericality_of :price, greater_than: 0
 end
