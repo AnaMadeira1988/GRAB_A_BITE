fab1f65cfd6591b7f96c56995fc6e4527dd5db16
Andre Costa <andrecondecosta@gmail.com>
2024-02-19 23:24:05 +0000

    add filter index

--- app/controllers/bites_controller.rb
+++ app/controllers/bites_controller.rb
@@ -5,8 +5,43 @@ class BitesController < ApplicationController
   def index
     @bites = Bite.all
     @bite = Bite.new
-    @bite_1 = Bite.first
-    @user_1 = User.first
+
+    if params[:date_start].present? && params[:date_end].present?
+      @bites = @bites.where('date BETWEEN ? AND ?', params[:date_start],params[:date_end])
+    end
+
+    if params[:dietary_options].present?
+      @bites = @bites.where(dietary_options: params[:dietary_options])
+    end
+
+    if params[:price].present?
+      @bites = @bites.where(price: params[:price])
+    end
+
+    if params[:meal_type].present?
+      @bites = @bites.where(meal_type: params[:meal_type])
+    end
+
+    if params[:local_drinks].present?
+      @bites = @bites.where(local_drinks: params[:local_drinks] == '1')
+    end
+
+    if params[:dessert].present?
+      @bites = @bites.where(dessert: params[:dessert] == '1')
+    end
+
+    if params[:number_of_guests].present?
+      @bites = @bites.where(number_of_guests: params[:number_of_guests])
+    end
+
+    if params[:city].present?
+      @bites = @bites.where(city: params[:city])
+    end
+
+    # if params[:host].present?
+    #   @bites = User.where(first_name: params[:host]).bites
+    # end
+
   end
 
   def create
@@ -28,6 +63,7 @@ class BitesController < ApplicationController
       render :edit, status: 422
     end
   end
+
 
   private
 
