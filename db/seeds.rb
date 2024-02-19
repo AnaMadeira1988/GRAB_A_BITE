1d9825178963e56702417f0b4f96358b1ac6b1ba
Ahmed Bouguacha <ahmad.bougacha@gmail.com>
2024-02-19 22:06:25 +0100

    Updated forms, added booking feature

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Bite.destroy_all
+15.times do
+  bite_params = {name: Faker::Food.dish,
+                  date: Faker::Date.between(from: Date.today + 1, to: 2.weeks.from_now),
+                  dietary_options: ['vegan', 'vegetarian', 'low_carb','halal', 'gluten_free', 'keto', 'lactose_free', 'kosher'].sample(3).join(' '),
+                  price: rand(5.2...29.99).round(2),
+                  meal_type: ['Breakfast', 'Lunch', 'Brunch', 'Dinner', 'Dessert', 'Snack', 'Drinks', 'Other'].sample,
+                  local_drinks: Faker::Boolean.boolean,
+                  dessert: Faker::Boolean.boolean,
+                  number_of_guests: rand(1..10),
+                  local_experience: Faker::Lorem.paragraph(sentence_count:4),
+                  city: Faker::Address.city,
+                  address: Faker::Address.street_address,
+                  accessibility: ['elevator', 'ramp', 'ground_floor'].sample(2).join(' ')
+                }
+  user = User.all.sample
+  puts "#{user.first_name} #{user.last_name} created a yummy bite!"
+  Bite.create!(bite_params.merge(user: user))
+end
+
+puts "Seeds created!"
