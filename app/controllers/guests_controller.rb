03a56399b56cb4d7130eb9181e1b29a46630e1e0
Ahmed Bouguacha <ahmad.bougacha@gmail.com>
2024-02-21 01:20:17 +0100

    Made progress on the booking page

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,7 +1,21 @@
 class GuestsController < ApplicationController
+  before_action :set_guest, only: %i[update destroy]
+
+  def update
+    @guest = Guest.find(params[:id])
+    @guest.update(confirmed: true)
+    redirect_to bite_path(@guest.bite), notice: "Booking has been successfully confirmed"
+  end
+
   def destroy
     @guest = Guest.find(params[:id])
     @guest.destroy
     redirect_to bite_path(@guest.bite), notice: "Booking has been successfully cancelled"
+  end
+
+  private
+
+  def set_guest
+    @guest = Guest.find(params[:id])
   end
 end
